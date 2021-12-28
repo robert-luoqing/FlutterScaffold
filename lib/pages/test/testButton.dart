9282ed6d62fad8f2a3861addbdf749b09d00fe6e
robert <robert@bitguild.com>
2021-12-28 18:35:16 +0800

    Update testButton.dart

--- lib/pages/test/testButton.dart
+++ lib/pages/test/testButton.dart
@@ -17,103 +17,105 @@ class _TestButtonState extends State<TestButton> {
     return SPScaffold(
         title: Text("Test Button"),
         body: Container(
-          color: Color(0x11000000),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  child: SPButton(
-                    pattern: SPButtonPattern.primaryRoundButton,
-                    text: "Select Button Size (${buttonSize.name})",
-                    fitContent: true,
-                    onPressed: () async {
-                      var newSize = await SPPicker.show(
-                          context,
-                          {
-                            SPButtonSize.small: "Small",
-                            SPButtonSize.middle: "Middle",
-                            SPButtonSize.large: "Large"
-                          },
-                          selectedKey: buttonSize);
-                      if (newSize != null) {
-                        setState(() {
-                          buttonSize = newSize;
-                        });
-                      }
-                    },
+          // color: Color(0x11000000),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    child: SPButton(
+                      pattern: SPButtonPattern.primaryRoundButton,
+                      text: "Select Button Size (${buttonSize.name})",
+                      fitContent: true,
+                      onPressed: () async {
+                        var newSize = await SPPicker.show(
+                            context,
+                            {
+                              SPButtonSize.small: "Small",
+                              SPButtonSize.middle: "Middle",
+                              SPButtonSize.large: "Large"
+                            },
+                            selectedKey: buttonSize);
+                        if (newSize != null) {
+                          setState(() {
+                            buttonSize = newSize;
+                          });
+                        }
+                      },
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SPButton(
-                  pattern: SPButtonPattern.primaryRoundButton,
-                  size: buttonSize,
-                  text: "Primary Round Button",
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SPButton(
+                    pattern: SPButtonPattern.primaryRoundButton,
+                    size: buttonSize,
+                    text: "Primary Round Button",
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SPButton(
-                  pattern: SPButtonPattern.primaryRoundButton,
-                  disabled: true,
-                  size: buttonSize,
-                  text: "Primary Round Disabled Button",
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SPButton(
+                    pattern: SPButtonPattern.primaryRoundButton,
+                    disabled: true,
+                    size: buttonSize,
+                    text: "Primary Round Disabled Button",
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SPButton(
-                  pattern: SPButtonPattern.primaryRectButton,
-                  size: buttonSize,
-                  text: "Primary Round Button",
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SPButton(
+                    pattern: SPButtonPattern.primaryRectButton,
+                    size: buttonSize,
+                    text: "Primary Round Button",
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SPButton(
-                  pattern: SPButtonPattern.primaryRectButton,
-                  disabled: true,
-                  size: buttonSize,
-                  text: "Primary Round Disabled Button",
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SPButton(
+                    pattern: SPButtonPattern.primaryRectButton,
+                    disabled: true,
+                    size: buttonSize,
+                    text: "Primary Round Disabled Button",
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SPButton(
-                  pattern: SPButtonPattern.primaryOutlineButton,
-                  size: buttonSize,
-                  text: "Primary Outline Button",
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SPButton(
+                    pattern: SPButtonPattern.primaryOutlineButton,
+                    size: buttonSize,
+                    text: "Primary Outline Button",
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SPButton(
-                  pattern: SPButtonPattern.primaryOutlineButton,
-                  disabled: true,
-                  size: buttonSize,
-                  text: "Primary Outline Disabled Button",
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SPButton(
+                    pattern: SPButtonPattern.primaryOutlineButton,
+                    disabled: true,
+                    size: buttonSize,
+                    text: "Primary Outline Disabled Button",
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SPButton(
-                  pattern: SPButtonPattern.secondOutlineButton,
-                  size: buttonSize,
-                  text: "Second Outline Button",
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SPButton(
+                    pattern: SPButtonPattern.secondOutlineButton,
+                    size: buttonSize,
+                    text: "Second Outline Button",
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SPButton(
-                  pattern: SPButtonPattern.secondOutlineButton,
-                  disabled: true,
-                  size: buttonSize,
-                  text: "Second Outline Disabled Button",
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SPButton(
+                    pattern: SPButtonPattern.secondOutlineButton,
+                    disabled: true,
+                    size: buttonSize,
+                    text: "Second Outline Disabled Button",
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ));
   }
