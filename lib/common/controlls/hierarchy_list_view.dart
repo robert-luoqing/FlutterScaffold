838a505363b7b5e2f3a3ac190001b4453a43b7fb
robert-bitguild <54098389+robert-bitguild@users.noreply.github.com>
2022-03-05 15:56:07 +0800

    Create hierarchy_list_view.dart

---
+++ lib/common/controlls/hierarchy_list_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// The widget is used to wrap Hierarchy list
+/// Like comment, reply comment etc.
+class HierarchyListView extends StatefulWidget {
+  const HierarchyListView({ Key? key }) : super(key: key);
+
+  @override
+  State<HierarchyListView> createState() => _HierarchyListViewState();
+}
+
+class _HierarchyListViewState extends State<HierarchyListView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
